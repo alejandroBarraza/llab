b856c9e1d9031b5200d7722cd137ac78258bcd61
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-08-21 20:17:29 -0400

    feat: Added Task controllers and routes

---
+++ backend/controllers/task.controller.js
@@ -0,0 +1,65 @@
+import { Task } from '../model/Task.js'
+
+// get all the tasks.
+export async function getTasks(req, res) {
+  try {
+    const tasks = await Task.findAll({
+      order: [['id', 'DESC']],
+    })
+    res.json(tasks)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
+// create a new task
+export async function createTask(req, res) {
+  try {
+    const { description, dueDate } = req.body
+    const newTask = await Task.create({
+      description,
+      dueDate,
+    })
+    res.json(newTask)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
+// delete a task by id
+export async function deleteTaskById(req, res, next) {
+  try {
+    const { id } = req.params
+    const task = await Task.findByPk(id)
+
+    if (!task) {
+      return res.status(404).json({ message: 'Tarea no encontrada' })
+    }
+
+    await task.destroy()
+
+    res.json({ message: 'Tarea eliminada con éxito' })
+  } catch (error) {
+    next(error)
+  }
+}
+
+//update a task by id
+export async function updateTaskById(req, res, next) {
+  try {
+    const { id } = req.params
+    const { name, done } = req.body
+
+    const task = await Task.findByPk(id)
+
+    if (!task) {
+      return res.status(404).json({ message: 'Tarea no encontrada' })
+    }
+
+    await task.update({ name, done })
+
+    res.json({ message: 'Tarea actualizada con éxito' })
+  } catch (error) {
+    next(error)
+  }
+}
---
+++ backend/routes/task.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import {
+  getTasks,
+  createTask,
+  deleteTaskById,
+  updateTaskById,
+} from '../controllers/task.controller.js'
+const router = Router()
+
+router.get('/task,', getTasks)
+router.post('/task', createTask)
+router.put('/task/:id', updateTaskById)
+router.delete('/task/:id', deleteTaskById)
+
+export default router
