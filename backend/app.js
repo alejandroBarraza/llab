77ea32e1301d9b2bf61869414aae00dd15202326
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-08-23 18:37:07 -0400

    chore: imported cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
 
 // Import routes
 import taskRoutes from './routes/task.routes.js'
 
 // Middlewares
+app.use(cors())
 app.use(express.json())
 
 // Routes
