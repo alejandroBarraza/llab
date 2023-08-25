9a9ba24e8558c9e19e478265b56c20f9e8b3cdb1
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2023-08-25 10:55:26 -0400

    feat: Add app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import React from 'react'
+import ItemList from './components/itemList'
+import { Container } from '@mui/material'
+import { Header } from './components/Header'
+import { format } from 'date-fns'
+import { ActionBar } from './components/ActionBar'
+import { AdditemButton } from './components/AdditemButton'
+import { es } from 'date-fns/locale'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const currentDate = new Date()
+  const formattedDate = format(currentDate, 'dd/MM/yyyy', { locale: es })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth='md'>
+      <Header currentDate={formattedDate}>Cosas por hacer</Header>
+      <ActionBar />
+      <ItemList />
+      <AdditemButton />
+    </Container>
   )
 }
 
